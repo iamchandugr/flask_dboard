3e57cb6bb175d757e964504b29419e59f507fc07
iamchandugr <chandrashekargr1995@gmail.com>
2021-07-23 08:29:11 +0530

    second

--- dashboard_test/script_api.py
+++ dashboard_test/script_api.py
@@ -3,6 +3,7 @@ listofendpoints = ['hello/','test1/', 'test2/', 'test3/', 'test4/']
 for i in listofendpoints:
     s  = "http://localhost:5000/contact/"+i
     response = requests.get(s)
-    print(response)
+
     #print("http://localhost:5000/contact/"+i)
     response = requests.get("http://localhost:5000/home/"+i)
+    print(response)
